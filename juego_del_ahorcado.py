1324b2096cd4d04afdf817ed7d8c50665127205c
Martin Cruz <martin_crdev@outloock.com>
2021-06-10 16:00:14 -0500

    Leemos el archivo data.txt y lo guardamos en una lista ennumerada con enumerate

--- juego_del_ahorcado.py
+++ juego_del_ahorcado.py
@@ -1,5 +1,13 @@
+def read():
+    palabras = []
+    with open("./archivos/data.txt", "r", encoding="utf-8") as f:
+        for i, palabra in enumerate(f):
+            palabras.append(palabra)
+    print(palabras[169])
+
+
 def run():
-    pass
+    read()
 
 if __name__ == "__main__":
     run()
